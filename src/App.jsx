463de3dbac0136f76d372dbe5ab59700d82f2385
danieltalhaug <daniel.talhaug@outlook.com>
2024-12-08 16:03:26 +0100

    Create job listings section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import AppNavbar from './components/AppNavbar';
 import AppHero from './components/AppHero';
 import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
 
 const App = () => {
 	return (
 		<>
 			<AppNavbar />
 			<AppHero />
+			<HomeCards />
+			<JobListings />
 		</>
 	);
 };
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,55 @@
+import Container from './Container';
+import Button from './Button';
+import Card from './Card';
+import jobs from '../jobs.json';
+
+const shuffleArray = array => {
+    const suffledArray = array;
+
+    for (let i = suffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return suffledArray;
+};
+
+function getRandomJobs() {
+    const availableJobs = shuffleArray(jobs);
+    const selectedJobs = [];
+    const limit = 9;
+
+    for (let i = 0; i < limit; i++) {
+        selectedJobs.push(availableJobs[i]);
+    }
+
+    return selectedJobs;
+}
+
+const JobListings = () => {
+	return (
+        <Container el="article">
+            <header className="py-16 text-center">
+                <h2 className="text-3xl font-bold">
+                    Browse jobs
+                </h2>
+            </header>
+            <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {getRandomJobs().map((job) => (
+                    <Card
+                        key={job.id}
+                        title={job.title}
+                        subtitle={job.description}
+                        theme="light"
+                    >
+                        <Button text="Read more" color="primary" outline />
+                    </Card>
+                ))}
+            </section>
+        </Container>
+	);
+};
+
+export default JobListings;
