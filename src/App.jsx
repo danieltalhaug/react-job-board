cc4335dd7a8bd5dd6c399699e4720e6256b57deb
danieltalhaug <daniel.talhaug@outlook.com>
2024-12-08 16:03:16 +0100

    Create home cards section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AppNavbar from './components/AppNavbar';
 import AppHero from './components/AppHero';
+import HomeCards from './components/HomeCards';
 
 const App = () => {
 	return (
---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,49 @@
+import Container from './Container';
+import Card from './Card';
+import Button from './Button';
+
+const cards = [
+    {
+        title: 'For clowns',
+        subtitle: 'Browse our gigs to find the prfect job for you.',
+        action: {
+            text: 'Browse jobs',
+            color: 'primary',
+            href: '',
+        }
+    },
+    {
+        title: 'For employers',
+        subtitle: 'List your job to hire a clown!',
+        action: {
+            text: 'Add job',
+            color: 'secondary',
+            href: '',
+        }
+    }
+];
+
+const HomeCards = () => {
+	return (
+        <div data-theme="light" className="py-32">
+            <Container>
+                <div className="flex gap-4 flex-col md:flex-row">
+                    {cards.map(card => (
+                        <Card
+                            key={card.title}
+                            title={card.title}
+                            subtitle={card.subtitle}
+                        >
+                            <Button
+                                text={card.action.text}
+                                color={card.action.color}
+                            />
+                        </Card>
+                    ))}
+                </div>
+            </Container>
+        </div>
+	);
+};
+
+export default HomeCards;
