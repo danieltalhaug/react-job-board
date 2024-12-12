c872294fcd8476ef2ff896b2495f4ef393d2deb4
danieltalhaug <daniel.talhaug@outlook.com>
2024-12-12 13:37:26 +0100

    Scroll to top when navigating to new pages

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import AppNavbar from '../components/AppNavbar';
 import AppFooter from '../components/AppFooter';
+import ScrollToTop from '../components/ScrollToTop';
 
 const DefaultLayout = () => {
     return (
         <>
+            <ScrollToTop />
             <AppNavbar />
             <Outlet />
             <AppFooter />
