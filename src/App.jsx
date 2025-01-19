df925b8cbbdc16bed6567e94f2bd52ababbc48e7
danieltalhaug <daniel.talhaug@outlook.com>
2025-01-19 12:21:24 +0100

    Create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import DefaultLayout from './layouts/DefaultLayout';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<DefaultLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path="/jobs" element={<JobsPage />} />
+			<Route path="*" element={<NotFoundPage />} />
 		</Route>
 	)
 );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+const NotFoundPage = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <span className="text-3xl">🤡</span>
+                    <h1 className="mt-4 text-5xl font-bold">404</h1>
+                    <p className="py-6">
+                        Looks like we clowned it and are unable to find the page you were looking for
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
